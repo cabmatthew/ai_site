4e2ad870b922ea2c654663e5bc9e9343e3843e64
mcaballero <mcaballero3@luc.edu>
2024-03-17 15:11:22 -0500

    main shows all models with some fields

--- my_site/education/views.py
+++ my_site/education/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Course, Module, Lab, Submission, Assignment
 
 def education(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    myCourses = Course.objects.all().values()
+    myModules = Module.objects.all().values()
+    myLabs = Lab.objects.all().values()
+    mySubmissions = Submission.objects.all().values()
+    myAssignments = Assignment.objects.all().values()
+
+    context = {
+        'myCourses': myCourses,
+        'myModules': myModules,
+        'myLabs': myLabs,
+        'mySubmissions': mySubmissions,
+        'myAssignments': myAssignments
+    }
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render(context, request))
